2192c1d6130c63a9e49e7e648ddba94f5beaae3b
Eric Tulowetzke <[ericwolf1998@gmail.com]>
2021-06-16 11:48:26 -0500

    update string db

--- shinyapp/go65/global.R
+++ shinyapp/go65/global.R
@@ -48,7 +48,7 @@ library(visNetwork)
 
 # relative path to data files
 datapath = "../../data/data103/"   # production server
-
+STRING_DB_VERSION <- "11.0" # what version of STRINGdb needs to be used 
 Min_overlap <- 1
 minSetSize = 3;
 mappingCoverage = 0.60 # 60% percent genes has to be mapped for confident mapping
