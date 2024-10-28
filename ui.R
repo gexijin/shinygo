6f87776d317330120317cceb6a4015cfd60b0083
Steven Ge <xijin.ge@sdstate.edu>
2024-10-28 10:38:31 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -282,8 +282,8 @@ ui <- fluidPage(
                 )
               )
             ),
-            p("10/26/24: v0.81. Diabled the switch of species during analysis. Fixed errors with STRING tab when STRINGdb species are used. 
-            If a user ID maps multiple Ensembl genes, all are kept for enrichment. "),
+            p("10/26/24: v0.81. Disabled the switch of species during analysis. Fixed errors with STRING tab when STRINGdb species are used. 
+            If a gene ID maps multiple Ensembl genes, all are kept for enrichment. "),
             p("10/25/24: Migrated to new server. Upgraded R to 4.4.0."),
 
             p("4/21/2024: New barchart with GO terms on the bars."),
