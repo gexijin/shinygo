94cdfd20b32c3a9f53ea60dc8c027cdd5d203460
Steven Ge <xijin.ge@sdstate.edu>
2025-04-26 17:25:15 -0500

    Chart module clean up

--- server.R
+++ server.R
@@ -728,247 +728,28 @@ server <- function(input, output, session) {
   )
 
  
-  # ggplot2 object for the enrichment chart;
-  # used both for display and download
-  enrichChartObject <- reactive({
-    if (input$goButton == 0) {
-      return()
-    }
-
-    if (is.null(significantOverlaps())) {
-      return(NULL)
-    }
-    if (ncol(significantOverlaps()$x) == 1) {
-      return(NULL)
-    } # no significant ones found.
-    tem <- input$selectOrg
-    tem <- input$SortPathwaysPlot
-    tem <- input$SortPathwaysPlotX
-    tem <- input$SortPathwaysPlotSize
-    tem <- input$SortPathwaysPlotColor
-    tem <- input$SortPathwaysPlotFontSize
-    tem <- input$SortPathwaysPlotMarkerSize
-    tem <- input$SortPathwaysPlotHighColor
-    tem <- input$SortPathwaysPlotLowColor
-    tem <- input$enrichChartType
-    tem <- input$enrichChartAspectRatio
-    tem <- input$maxTerms
-    tem <- input$abbreviatePathway
-    req(input$ggplot2_theme)
-    tem <- input$show_pathway_id
-
-    isolate({
-      goTable <- significantOverlaps()$x[, 1:5]
-
-      # Remove spaces in col names
-      colnames(goTable) <- gsub(" ", "", colnames(goTable))
-
-
-      x <- input$SortPathwaysPlotX
-      size <- input$SortPathwaysPlotSize
-      colorBy <- input$SortPathwaysPlotColor
-      fontSize <- input$SortPathwaysPlotFontSize
-      markerSize <- input$SortPathwaysPlotMarkerSize
-      # validate values; users can input any numeric value outside the range
-      if (fontSize < 1 | fontSize >= 20) {
-        fontSize <- 12
-      }
-      if (markerSize < 0 | markerSize > 20) {
-        markerSize <- 4
-      }
-
-      # convert to vector so that we can look up the readable names of columns
-      columns <- unlist(columnSelection)
-
-      goTable$EnrichmentFDR <- -log10(goTable$EnrichmentFDR)
-      ix <- which(colnames(goTable) == input$SortPathwaysPlot)
-
-      # sort the pathways
-      if (ix > 0 && ix < dim(goTable)[2]) {
-        goTable <- goTable[order(goTable[, ix], decreasing = TRUE), ]
-      }
-
-      # remove pathway ID  only in Ensembl species
-      if (!input$show_pathway_id && as.integer(input$selectOrg) > 0) {
-        goTable$Pathway <- remove_pathway_id(goTable$Pathway, input$selectGO)
-      }
-
-      # Error when two pathways are of the same name due to truncation of long pathways
-      goTable$Pathway <- mark_duplicates(goTable$Pathway)
-
-      # convert to factor so that the levels are not reordered by ggplot2
-      goTable$Pathway <- factor(goTable$Pathway, levels = rev(goTable$Pathway))
-
-      p <- ggplot(goTable, aes_string(x = x, y = "Pathway", size = size, color = colorBy)) +
-        geom_point() +
-        scale_color_continuous(
-          low = input$SortPathwaysPlotLowColor,
-          high = input$SortPathwaysPlotHighColor,
-          name = names(columns)[columns == colorBy],
-          guide = guide_colorbar(reverse = TRUE)
-        )
-
-      p <- refine_ggplot2(
-        p = p,
-        gridline = FALSE,
-        ggplot2_theme = input$ggplot2_theme
-      )
-
-      p <- p + scale_size(range = c(1, markerSize)) +
-        xlab(names(columns)[columns == x]) +
-        ylab(NULL) +
-        guides(
-          size = guide_legend(order = 2, title = names(columns)[columns == size]),
-          color = guide_colorbar(order = 1)
-        ) +
-        theme(axis.text = element_text(size = fontSize), axis.title = element_text(size = 12)) +
-        theme(
-          legend.title = element_text(size = 12), # decrease legend font
-          legend.text = element_text(size = 12)
-        ) +
-        guides(shape = guide_legend(override.aes = list(size = 5))) +
-        guides(color = guide_legend(override.aes = list(size = 5)))
-
-      if (input$enrichChartType == "dotplot") {
-        p <- p
-      } else if (input$enrichChartType == "lollipop") {
-        p <- p +
-          geom_segment(aes_string(
-            x = 0,
-            xend = x,
-            y = "Pathway",
-            yend = "Pathway"
-          ),
-          size = 1
-          )
-      } else if (input$enrichChartType == "barplot") {
-        p <- ggplot(goTable, aes_string(x = x, y = "Pathway", fill = colorBy)) +
-          geom_col(width = 0.8, position = position_dodge(0.7)) +
-          scale_fill_continuous(
-            low = input$SortPathwaysPlotLowColor,
-            high = input$SortPathwaysPlotHighColor,
-            name = names(columns)[columns == colorBy],
-            guide = guide_colorbar(reverse = TRUE)
-          )
-        p <- refine_ggplot2(
-          p = p,
-          gridline = FALSE,
-          ggplot2_theme = input$ggplot2_theme
-        )
-        p <- p +
-          xlab(names(columns)[columns == x]) +
-          ylab(NULL) +
-          theme(axis.text = element_text(size = fontSize))
-      } else if(input$enrichChartType == "barplot_inside") {
-        p <- ggplot(goTable, aes_string(x = x, y = "Pathway", fill = colorBy)) +
-          geom_col(width = 0.8, position = position_dodge(0.7)) +
-          scale_fill_continuous(
-            low = input$SortPathwaysPlotLowColor,
-            high = input$SortPathwaysPlotHighColor,
-            name = names(columns)[columns == colorBy],
-            guide = guide_colorbar(reverse = TRUE)
-          )
-        p <- refine_ggplot2(
-          p = p,
-          gridline = FALSE,
-          ggplot2_theme = input$ggplot2_theme
-        )
-        p <- p +
-          xlab(names(columns)[columns == x]) +
-          ylab(NULL) +
-          theme(axis.text = element_text(size = fontSize))+
-          geom_text( # add text inside the bars
-            aes_string(x = 0, label = "Pathway"),
-            hjust = 0, 
-            #vjust = -1, 
-            color="black", 
-            size=fontSize / 2
-          )  +
-          theme(
-            axis.title.y=element_blank(),
-            axis.ticks.y=element_blank(),
-            axis.text.y=element_blank()
-          )
-
-      }
-
-      return(p)
-    }) # isolate
-  })
-
-  observeEvent(input$enrichChartType, {
-    req (input$enrichChartType == "barplot_inside") 
-    updateSliderInput(
-      inputId = "SortPathwaysPlotFontSize",
-      value = 10
-    )
-    updateSliderInput(
-      inputId = "enrichChartAspectRatio",
-      value = 1.5
-    )
-    updateSelectInput(
-      inputId = "SortPathwaysPlotLowColor",
-      selected = "yellow"
-    )
-    updateSelectInput(
-      inputId = "maxTerms",
-      selected = "15"
-    )
-    #show notification that maxTerms is set to 15
-    showNotification(
-      "To improve the chart, we adjusted the aspect ratio, font size, and color:low. The number of pathways to show is set to 15.",
-      type = "message",
-      duration = 5
-    )
-  })
-
-
-  # Enrichment plot for display on the screen
-  # https://stackoverflow.com/questions/34792998/shiny-variable-height-of-renderplot
-  output$enrichChart <- renderPlot(
-    {
-      enrichChartObject()
-    },
-    # height increases as the number of terms increase. max at 1200, min 350
-    height = function() {
-      round(max(350, min(2500, round(18 * as.numeric(input$maxTerms)))))
-    },
-    width = function() {
-      round(max(350, min(2500, round(18 * as.numeric(input$maxTerms)))) * as.numeric(input$enrichChartAspectRatio))
-    }
-  )
-
-  download_barplot <- mod_download_images_server(
-    "download_barplot",
-    filename = "barplot",
-    figure = reactive({
-      enrichChartObject()
-    }),
-    width = 8,
-    height = round(8 / as.numeric(input$enrichChartAspectRatio), 1)
-  )
 
     mod_tree_server("tree", 
-    significantOverlaps2 = reactive({
-      if (input$goButton == 0) {
-        return()
-      }
-      tem <- input$input_text_b # just to make it re-calculate if user changes background
+      significantOverlaps2 = reactive({
+        if (input$goButton == 0) {
+          return()
+        }
+        tem <- input$input_text_b # just to make it re-calculate if user changes background
 
-      tem <- significantOverlaps()
-      if (dim(tem$x)[2] == 1) {
-        return(NULL)
-      }
-      tem <- tem$x
-      colnames(tem) <- c("adj.Pval", "nGenesList", "nGenesCategor", "Fold", "Pathways", "URL", "Genes")
-      tem$Pathways <- gsub(".*'_blank'>|</a>", "", tem$Pathways) # remove URL
-      tem$Direction <- "Diff"
-      # remove pathway ID only in Ensembl species
-      if (!input$show_pathway_id && as.integer(input$selectOrg) > 0) {
-        tem$Pathways <- remove_pathway_id(tem$Pathways, input$selectGO)
-      }
-      tem
-    }),
+        tem <- significantOverlaps()
+        if (dim(tem$x)[2] == 1) {
+          return(NULL)
+        }
+        tem <- tem$x
+        colnames(tem) <- c("adj.Pval", "nGenesList", "nGenesCategor", "Fold", "Pathways", "URL", "Genes")
+        tem$Pathways <- gsub(".*'_blank'>|</a>", "", tem$Pathways) # remove URL
+        tem$Direction <- "Diff"
+        # remove pathway ID only in Ensembl species
+        if (!input$show_pathway_id && as.integer(input$selectOrg) > 0) {
+          tem$Pathways <- remove_pathway_id(tem$Pathways, input$selectGO)
+        }
+        tem
+      }),
     input_goButton = reactive(input$goButton),
     input_maxTerms = reactive(input$maxTerms)
   )
